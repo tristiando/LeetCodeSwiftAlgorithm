acf152e37c59fc29ad696659fec75931d2986108
Thu Do <dothu.khtn@gmail.com>
2023-02-27 11:34:33 +0700

    add folder of 'Number of Islands' problem

---
+++ 200. Number of Islands/NumberOfIslands.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+
+
+func numIslands(_ grid: [[Character]]) -> Int {
+    let (rows, columns) = (grid.count, grid[0].count)
+    var visited = Array(repeating: Array(repeating: false, count: columns), count: rows)
+    var count = 0
+    
+    func dfs(_ r: Int, _ c: Int) {
+        guard r >= 0, c >= 0, r < rows, c < columns, grid[r][c] == "1", visited[r][c] == false else { return }
+        
+        visited[r][c] = true
+        dfs(r - 1, c)
+        dfs(r + 1, c)
+        dfs(r, c - 1)
+        dfs(r, c + 1)
+    }
+    
+    for r in 0..<rows {
+        for c in 0..<columns {
+            if grid[r][c] == "1" && visited[r][c] == false {
+                count += 1
+                dfs(r, c)
+            }
+        }
+    }
+    return count
+}
+
+// expect: 3
+print(numIslands([
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+  ]
+))
